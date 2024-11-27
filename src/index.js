53856b7209d670fee95ab10526cc052c29dbdf0e
unnati1004 <unnatigandhi999@gmail.com>
2024-11-27 20:34:20 +0530

    create login page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(App());
+
 root.render(
   <React.StrictMode>
     <App />
